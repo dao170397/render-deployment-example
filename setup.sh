08b89e4778279c5751205f16038bdcc61e1ed440
Dao Nguyen <dao1703songngu@gmail.com>
2024-06-19 21:41:46 +0700

    update database_url

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://postgres:sys@localhost:5432/heroku_sample"
+export DATABASE_URL="postgres://ud2qcpbpmj9dg6:p5f125721985182f79ac55e8b1445cc8a36ab5f888836a00ad3fc8d800173ffd1@c3gtj1dt5vh48j.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d2jjvnft9ql6ap"
 export EXCITED="true"
 echo "setup.sh script executed successfully!"
